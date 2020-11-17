ef139bf57f0564474d4bef2e701edaa9345d842b
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-17 00:54:30 -0600

    Completing text in other question routes and finishing styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+// import { promises } from 'dns';
+import { CallbacksComponent } from './callbacks/callbacks.component';
 import { CreatorComponent } from './creator/creator.component';
+import { MapComponent } from './map/map.component';
+import { PromisesComponent } from './promises/promises.component';
 
 const routes: Routes = [
   {
     path: 'creator',
     component: CreatorComponent
+  },
+  {
+    path: 'callbacks',
+    component: CallbacksComponent
+  },
+  {
+    path: 'map',
+    component: MapComponent
+  },
+  {
+    path: 'promises',
+    component: PromisesComponent
   }
 ];
 
