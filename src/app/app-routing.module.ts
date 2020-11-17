327d372ac8d4e2f5fb5211d7e4340bdff29040c5
fzramos <64625115+fzramos@users.noreply.github.com>
2020-11-17 00:14:25 -0600

    Skeleton and first route finished

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreatorComponent } from './creator/creator.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'creator',
+    component: CreatorComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CreatorComponent } from './creator/creator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreatorComponent
   ],
   imports: [
     BrowserModule,
